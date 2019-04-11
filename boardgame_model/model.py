aa98bfe25787e8e9318378e2551cb9ab49e12704
tommartinson <tomemartinson@gmail.com>
2019-04-10 22:32:30 -0700

    added console output

--- boardgame_model/model.py
+++ boardgame_model/model.py
@@ -1,7 +1,9 @@
 from mesa import Agent, Model
 from mesa.space import SingleGrid
 import random
-
+#Thomas Martinson
+#SDSU
+#CS558
 
 
 class BoardGameModel(Model):
@@ -44,17 +46,20 @@ where their rival lies, they capture the other and win.
         numBlue = len(self.blueAgentList)
         if ((numRed > 0) and (numBlue > 0)):
             if (self.turn == 1):
-                print("red turn")
+                print("Red Turn")
                 self.redAgentList[random.randint(0,numRed-1)].step()
             
                 self.turn = 0
             else:
-                print("blue turn")
+                print("Blue Turn")
                 self.blueAgentList[random.randint(0,numBlue-1)].step()
             
                 self.turn = 1
         else:
-            print("Game Over!")
+            if(numRed is 0):
+                print("Game Over! Blue Team Wins.")
+            else:
+                print("Game Over! Red Team Wins.")
             self.running = False
         
         
@@ -64,25 +69,26 @@ class RedPiece(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         
-    
-    
     def move(self):
         
         possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
         new_position = self.random.choice(possible_steps)
         
         if self.model.grid.is_cell_empty(new_position): # check if cell is empty
+            print("Red piece moves from ",self.pos," to ",new_position)
             self.model.grid.move_agent(self, new_position) # move agent to empty cell
+            
         else: # check if cell has a blue agent
             cellmates = self.model.grid.get_cell_list_contents([new_position])
             if type(cellmates[0]) is BluePiece:
                 
                 self.model.grid.remove_agent(cellmates[0])
                 self.model.blueAgentList.remove(cellmates[0])
-                print(len(self.model.blueAgentList))
+                print("Red piece at",self.pos,"killed blue piece at ",new_position)
+                self.model.grid.move_agent(self, new_position) # move agent to empty cell
+            else:
+                print("Red piece attempted to move from",self.pos,"to",new_position,", red piece already there.")
             
-            
-
     def step(self):
         self.move()
         
@@ -93,21 +99,25 @@ class BluePiece(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         
-
-    #MODIFY MOVE FUNCTION
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
         new_position = self.random.choice(possible_steps)
         
         if self.model.grid.is_cell_empty(new_position): # check if cell is empty
+            print("Blue piece moves from ",self.pos," to ",new_position)
             self.model.grid.move_agent(self, new_position) # move agent to empty cell
+            
         else: # check if cell has a red agent
             cellmates = self.model.grid.get_cell_list_contents([new_position])
             if type(cellmates[0]) is RedPiece:
                 
                 self.model.grid.remove_agent(cellmates[0])
                 self.model.redAgentList.remove(cellmates[0])
-                print(len(self.model.redAgentList))
+                print("Blue piece at",self.pos,"killed red piece at ",new_position)
+                self.model.grid.move_agent(self, new_position) # move agent to empty cell
+            else:
+                print("Blue piece attempted to move from",self.pos,"to",new_position,", blue piece already there.")
+                
 
     def step(self):
         self.move()
